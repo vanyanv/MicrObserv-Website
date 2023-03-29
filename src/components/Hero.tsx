e240d84a20b104512feea6914944fe92576a4ccf
Vardan <vardan.vanyan@gmail.com>
2023-03-29 11:42:54 -0700

    finished most of hero page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
+import dashboard from '../../public/dashboard.jpg';
 import PropTypes from 'prop-types';
 import { ChevronRightIcon } from '@heroicons/react/20/solid';
 
@@ -66,12 +68,15 @@ export default function Hero() {
             alt='Your Company'
           />
           <div className='mt-24 sm:mt-32 lg:mt-16'>
-            <a href='#' className='inline-flex space-x-6'>
+            <a
+              href='https://github.com/oslabs-beta/MicrObserv'
+              className='inline-flex space-x-6'
+            >
               <span className='rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20'>
                 What's new
               </span>
               <span className='inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300'>
-                <span>Just shipped v1.0</span>
+                <span>v1.0</span>
                 <ChevronRightIcon
                   className='h-5 w-5 text-gray-500'
                   aria-hidden='true'
@@ -80,29 +85,32 @@ export default function Hero() {
             </a>
           </div>
           <h1 className='mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl'>
-            Deploy to the cloud with confidence
+            MicrObserv
           </h1>
           <p className='mt-6 text-lg leading-8 text-gray-300'>
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+            MicrObserv is here to simplify observability for your microservice
+            architecture by consolidating all logs and tracers into one
+            centralized database.
           </p>
           <div className='mt-10 flex items-center gap-x-6'>
             <a
-              href='#'
+              href='https://github.com/oslabs-beta/MicrObserv'
               className='rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400'
             >
-              Get started
+              Documentation
             </a>
-            <a href='#' className='text-sm font-semibold leading-6 text-white'>
-              Learn more <span aria-hidden='true'>→</span>
+            <a
+              href='https://github.com/oslabs-beta/MicrObserv'
+              className='text-sm font-semibold leading-6 text-white'
+            >
+              To Github <span aria-hidden='true'>→</span>
             </a>
           </div>
         </div>
         <div className='mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mt-0 lg:mr-0 lg:max-w-none lg:flex-none xl:ml-32'>
           <div className='max-w-3xl flex-none sm:max-w-5xl lg:max-w-none'>
-            <img
-              src='https://tailwindui.com/img/component-images/dark-project-app-screenshot.png'
+            <Image
+              src={dashboard}
               alt='App screenshot'
               width={2432}
               height={1442}
