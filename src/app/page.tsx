546f953fa2e3eeced2d7442d09b035f9006df9cb
Vardan <vardan.vanyan@gmail.com>
2023-03-29 00:18:40 -0700

    added navbar and hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from './page.module.css';
-
+import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-      <h1 className=' text-3xl underline'>Hello world!</h1>
+      <Navbar />
+      <Hero />
     </>
   );
 }
