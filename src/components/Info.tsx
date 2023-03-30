26da86af1e8ab7effe3c065fcf08d6019cc7d395
Vardan <vardan.vanyan@gmail.com>
2023-03-30 12:04:13 -0700

    minor changes

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -12,42 +12,42 @@ import dashboard from '../../public/dashboard.jpg';
 import Image from 'next/image';
 
 const features = [
-  //   {
-  //     name: 'Push to deploy.',
-  //     description:
-  //       'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-  //     icon: CloudArrowUpIcon,
-  //   },
-  //   {
-  //     name: 'SSL certificates.',
-  //     description:
-  //       'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-  //     icon: LockClosedIcon,
-  //   },
-  //   {
-  //     name: 'Simple queues.',
-  //     description:
-  //       'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
-  //     icon: ArrowPathIcon,
-  //   },
-  //   {
-  //     name: 'Advanced security.',
-  //     description:
-  //       'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-  //     icon: FingerPrintIcon,
-  //   },
-  //   {
-  //     name: 'Powerful API.',
-  //     description:
-  //       'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-  //     icon: Cog6ToothIcon,
-  //   },
-  //   {
-  //     name: 'Database backups.',
-  //     description:
-  //       'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
-  //     icon: ServerIcon,
-  //   },
+  {
+    name: 'Push to deploy.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description:
+      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple queues.',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
+    icon: ArrowPathIcon,
+  },
+  {
+    name: 'Advanced security.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Powerful API.',
+    description:
+      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: Cog6ToothIcon,
+  },
+  {
+    name: 'Database backups.',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
+    icon: ServerIcon,
+  },
 ];
 
 export default function Info() {
@@ -87,7 +87,7 @@ export default function Info() {
       </div>
       <div className='mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8'>
         <dl className='mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16'>
-          {features.map((feature) => (
+          {/* {features.map((feature) => (
             <div key={feature.name} className='relative pl-9'>
               <dt className='inline font-semibold text-white'>
                 <feature.icon
@@ -98,7 +98,7 @@ export default function Info() {
               </dt>{' '}
               <dd className='inline'>{feature.description}</dd>
             </div>
-          ))}
+          ))} */}
         </dl>
       </div>
     </div>
