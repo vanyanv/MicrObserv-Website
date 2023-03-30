346a66aac327ab0a9a967a8c3d02d994b2ca7cf2
Vardan <vardan.vanyan@gmail.com>
2023-03-30 01:41:51 -0700

    added animations to technologies

--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -6,6 +6,39 @@ import tailwindcss from '../../public/logos/tailwindcss.svg';
 import webpack from '../../public/logos/webpack.svg';
 
 import Image from 'next/image';
+
+const technologies = [
+  {
+    src: electron,
+    alt: 'Electron',
+    width: 158,
+    height: 48,
+  },
+  {
+    src: typescript,
+    alt: 'Electron',
+    width: 158,
+    height: 48,
+  },
+  {
+    src: react,
+    alt: 'Electron',
+    width: 158,
+    height: 48,
+  },
+  {
+    src: node,
+    alt: 'Electron',
+    width: 158,
+    height: 48,
+  },
+  {
+    src: webpack,
+    alt: 'Electron',
+    width: 158,
+    height: 48,
+  },
+];
 export default function Logos() {
   return (
     <div className='bg-gray-900 py-24 sm:py-32'>
@@ -15,35 +48,35 @@ export default function Logos() {
         </h2>
         <div className='mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5'>
           <Image
-            className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
+            className='col-span-2 max-h-12 w-full object-contain lg:col-span-1 animate-bounce'
             src={electron}
             alt='Electron'
             width={158}
             height={48}
           />
           <Image
-            className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
+            className='col-span-2 max-h-12 w-full object-contain lg:col-span-1 animate-bounce'
             src={typescript}
             alt='TypeScript'
             width={158}
             height={48}
           />
           <Image
-            className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
+            className='col-span-2 max-h-12 w-full object-contain lg:col-span-1 animate-bounce'
             src={react}
             alt='React'
             width={158}
             height={48}
           />
           <Image
-            className='col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1'
+            className='col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1 animate-bounce'
             src={node}
             alt='node'
             width={158}
             height={48}
           />
           <Image
-            className='col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1'
+            className='col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1 animate-bounce'
             src={webpack}
             alt='Webpack'
             width={158}
